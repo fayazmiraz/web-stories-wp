6efee73e5f7184bb2f594d357fe04be33a8bdac0
Brittany Feenstra <brittany.feenstra@formidable.com>
2021-10-11 11:35:25 -0700

    remove redundant debounces

--- packages/story-editor/src/components/panels/document/taxonomies/HierarchicalTermSelector.js
+++ packages/story-editor/src/components/panels/document/taxonomies/HierarchicalTermSelector.js
@@ -32,7 +32,6 @@ import {
 } from '@web-stories-wp/design-system';
 import {
   useCallback,
-  useDebouncedCallback,
   useEffect,
   useMemo,
   useRef,
@@ -149,7 +148,6 @@ function HierarchicalTermSelector({
   const toggleRef = useRef();
   const [toggleFocus, setToggleFocus] = useState(false);
   const speak = useLiveRegion('assertive');
-  const debouncedSpeak = useDebouncedCallback(speak, 500, { leading: true });
 
   const resetInputs = useCallback(() => {
     setNewCategoryName('');
@@ -207,20 +205,20 @@ function HierarchicalTermSelector({
         slug: selectedParentSlug,
       };
       createTerm(taxonomy, newCategoryName, parentValue, true);
-      setShowAddNewCategory(false);
-      resetInputs();
-      setToggleFocus(showAddNewCategory);
-      debouncedSpeak(
+      speak(
         sprintf(
           /* Translators: %s: Taxonomy label name. */
           __('%s added.', 'web-stories'),
           taxonomy.labels.singular_name
         )
       );
+      setShowAddNewCategory(false);
+      resetInputs();
+      setToggleFocus(showAddNewCategory);
     },
     [
       createTerm,
-      debouncedSpeak,
+      speak,
       newCategoryName,
       noParentId,
       resetInputs,
